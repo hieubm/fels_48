e00798f13538e4c4d1693e242d657f47fa0a03b2
Cao Luc <cao.thanh.luc@framgia.com>
2015-04-09 13:35:03 +0700

    Sign in, sign up and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  get 'signup' => 'users#new'
+  post 'signup' => 'users#create'
+
+  get 'login'   => 'sessions#new'
+  post 'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  resources :users, only: [:show]
 end
